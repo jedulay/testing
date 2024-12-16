806ddc0102e95dc0ea5c055ae638a53fc0832db8
jedulay <dulayjenine@gmail.com>
2024-12-16 22:49:31 +0800

    fixed issues

--- java/script.js
+++ java/script.js
@@ -7,3 +7,14 @@ document.getElementById('clickableImage').addEventListener('click', function() {
         window.location.href = "home"; // Replace with your target page URL
     }, 500); // Match the duration of the flash animation
 });
+
+document.getElementById('clickableImage').addEventListener('click', function() {
+    const flash = document.getElementById('flash');
+    flash.classList.add('flash-animation');
+
+    // Wait for the animation to complete before navigating
+    setTimeout(() => {
+        window.location.href = 'home2.html'; // Replace with your target page URL
+    }, 3000); // Match the duration of the flash animation (3 seconds)
+});
+
